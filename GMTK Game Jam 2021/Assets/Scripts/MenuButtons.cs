0058bc38a101493d98bccb06315b75fe11639ca5
David B <31740816+fresh-brued@users.noreply.github.com>
2021-06-13 00:23:39 -0400

    Added menu audio

--- GMTK Game Jam 2021/Assets/Scripts/MenuButtons.cs
+++ GMTK Game Jam 2021/Assets/Scripts/MenuButtons.cs
@@ -5,35 +5,44 @@ using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
 {
+
+  public AudioSource click; 
     // Start is called before the first frame update
   public void play(GameObject levelMenu)
   {
+      click.Play(); 
       levelMenu.SetActive(true);
+     
 
   }
 
   public void controls (GameObject controlMenu)
   {
+      click.Play(); 
       controlMenu.SetActive(true);
   }
 
   public void exit()
   {
+      click.Play(); 
       Application.Quit(); 
   }
 
   public void back(GameObject buttonParent)
   {
+      click.Play(); 
       buttonParent.SetActive(false);
   }
 
   public void loadLevel(int i)
   {
+      click.Play(); 
       SceneManager.LoadScene(i);
   }
 
   public void restart()
   {
+      click.Play(); 
       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
   }
 }
