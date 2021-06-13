ecf4c20bd38ce60c81c848b2aa79e17caeae3739
David B <31740816+fresh-brued@users.noreply.github.com>
2021-06-12 21:38:15 -0400

    Added main menu

---
+++ GMTK Game Jam 2021/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtons : MonoBehaviour
+{
+    // Start is called before the first frame update
+  public void play(GameObject levelMenu)
+  {
+      levelMenu.SetActive(true);
+
+  }
+
+  public void controls (GameObject controlMenu)
+  {
+      controlMenu.SetActive(true);
+  }
+
+  public void exit()
+  {
+      Application.Quit(); 
+  }
+
+  public void back(GameObject buttonParent)
+  {
+      buttonParent.SetActive(false);
+  }
+}
